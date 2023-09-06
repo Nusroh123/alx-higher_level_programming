19c45c123951a49dc91394ff6d538832911cfb34
Nusroh123 <oyedelenusroh@gmail.com>
2023-09-06 16:18:41 -0700

    stringToUpperCase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    upperCase = ""
+    for char in str:
+        if 'a' <= char <= 'z':
+            upperCase += chr(ord(char) - 32)
+        else:
+            upperCase += char
+    print(upperCase)
