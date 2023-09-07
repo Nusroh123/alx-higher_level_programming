c94d5af9d1ef4c135241a5ec8d67ccac78d93c13
Nusroh123 <oyedelenusroh@gmail.com>
2023-09-07 05:07:27 -0700

    import

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -8,19 +8,7 @@ if __name__ == "__main__":
     mulRes = mul(a, b)
     divRes = div(a, b)
 
-    '''
-    print("{} + {} = {}".format(a, b, addRes))
-    print("{} + {} = {}".format(a, b, subRes))
-    print("{} + {} = {}".format(a, b, mulRes))
-    print("{} + {} = {}".format(a, b, divRes))
-    '''
-    '''
-    print(a + " + " + b + " = " + addRes)
-    print(a + " + " + b + " = " + subRes)
-    print(a + " + " + b + " = " + mulRes)
-    print(a + " + " + b + " = " + divRes)
-    '''
     print(f"{a} + {b} = {addRes}")
-    print(f"{a} + {b} = {subRes}")
-    print(f"{a} + {b} = {mulRes}")
-    print(f"{a} + {b} = {divRes}")
+    print(f"{a} - {b} = {subRes}")
+    print(f"{a} * {b} = {mulRes}")
+    print(f"{a} / {b} = {divRes}")
