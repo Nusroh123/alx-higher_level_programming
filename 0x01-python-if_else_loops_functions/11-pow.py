c19fa27f343beb153accc4f645f5fd1290061a73
Nusroh123 <oyedelenusroh@gmail.com>
2023-09-06 17:35:47 -0700

    pow of numbers

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def pow(a, b):
     res = 1
-    if b >= 0:
-        for i in range(b):
-            res *= a
-    else:
-        b *= -1
-        for i in range(b):
-            res *= 1 / a
+    if b < 0:
+        a = 1 / a
+        b = -b
+
+    for i in range(b):
+        res *= a
     return (res)
