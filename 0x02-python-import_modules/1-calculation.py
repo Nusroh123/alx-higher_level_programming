d9327c0643bbbf2839c6d5203e633958668c2810
Nusroh123 <oyedelenusroh@gmail.com>
2023-09-07 06:07:49 -0700

    import

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,7 +4,12 @@ if __name__ == "__main__":
     a = 10
     b = 5
 
-    print(f"{a} + {b} = {add(a, b)}")
-    print(f"{a} - {b} = {sub(a, b)}")
-    print(f"{a} * {b} = {mul(a, b)}")
-    print(f"{a} / {b} = {div(a, b)}")
+    addRes = add(a, b)
+    subRes = sub(a, b)
+    mulRes = mul(a, b)
+    divRes = div(a, b)
+
+    print(f"{a} + {b} = {addRes}")
+    print(f"{a} - {b} = {subRes}")
+    print(f"{a} * {b} = {mulRes}")
+    print(f"{a} / {b} = {divRes}")
