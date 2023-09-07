89ad18367bdba7c912d87c01fcde3744f88c75aa
Nusroh123 <oyedelenusroh@gmail.com>
2023-09-07 04:42:43 -0700

    import

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    addRes = add(a, b)
+    subRes = sub(a, b)
+    mulRes = mul(a, b)
+    divRes = div(a, b)
+    print("{} + {} = {}".format(a, b, addRes))
+    print("{} + {} = {}".format(a, b, subRes))
+    print("{} + {} = {}".format(a, b, mulRes))
+    print("{} + {} = {}".format(a, b, divRes))
