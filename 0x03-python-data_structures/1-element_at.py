b6764e8aeb49bb9c2cf51d6c4288441197bc99c1
Nusroh123 <oyedelenusroh@gmail.com>
2023-09-11 07:21:37 -0700

    index

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     listLen = len(my_list)
-    if idx < 0:
+    if 0 <= idx > listLen:
         return (None)
     currentIndex = 0
     for element in my_list:
