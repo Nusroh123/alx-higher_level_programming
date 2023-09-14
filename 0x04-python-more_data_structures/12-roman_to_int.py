aaba64011be303c4d9336a9879d1760fe0e9b251
Nusroh123 <oyedelenusroh@gmail.com>
2023-09-13 21:53:34 -0700

    roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,10 @@ def roman_to_int(roman_string):
     romanToInt = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     total = 0
     prevRoman = 0
-    
+
+    if type(roman_string) is not str or None:
+        return (0)
+
     for number in reversed(roman_string):
         roman = romanToInt[number]
         if roman < prevRoman:
